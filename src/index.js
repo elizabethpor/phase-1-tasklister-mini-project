b46eedb3fa7c023af99176f560e45d628cae9f90
Elizabeth <elizabeth.por@gmail.com>
2022-02-07 22:42:35 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildTodo(e.target['new-task-description'].value);
+    form.reset();
+  })
 });
+
+function buildTodo(todo) {
+  let p = document.createElement('p');
+  p.textContent = `${todo} `
+  let deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'x';
+  deleteBtn.addEventListener('click', handleDelete);
+  p.appendChild(deleteBtn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+};
